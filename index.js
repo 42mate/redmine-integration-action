966998a6b900cb85d1a6905b866e76d78e168188
wmb1207 <wmb1207@proton.me>
2025-03-18 17:06:47 -0300

    wip

--- index.js
+++ index.js
@@ -36,62 +36,13 @@ async function run() {
       },
     };
 
-    // var req = http.put(
-    //   "https://redmine.42mate.com/issues/9196.json",
-    //   params,
-    //   (res) => {
-    //     console.log(res);
-    //   },
-    // );
-
-    // // Define the options object
-    // const options = {
-    //   method: 'PUT',
-    //   hostname: 'example.',
-    //   path: '/api/users/1',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //     'Authorization': 'Bearer <your-token>'
-    //   }
-    // };
-
-    // // Define the body
-    // const body = JSON.stringify({ name: 'Bob' });
-
-    // // Create the request
-    // const req = http.request(options, (res) => {
-    //   // Handle the response
-    //   console.log(`Status code: ${res.statusCode}`);
-    //   console.log(`Headers: ${JSON.stringify(res.headers)}`);
-    //   let data = '';
-    //   res.on('data', (chunk) => {
-    //     // Concatenate the data chunks
-    //     data += chunk;
-    //   });
-    //   res.on('end', () => {
-    //     // Parse the data as JSON
-    //     data = JSON.parse(data);
-    //     console.log(`Data: ${JSON.stringify(data)}`);
-    //   });
-    // });
-
-    // // Handle the error
-    // req.on('error', (err) => {
-    //   console.error(`Error: ${err.message}`);
-    // });
-
-    // // Write the body to the request
-    // req.write(body);
-
-    // // End the request
-    // req.end();
-
     var req = http.request(options, function (res) {
       if (
         res.statusCode !== 200 &&
         res.statusCode !== 201 &&
         res.statusCode !== 204
       ) {
+        console.log(res.statusCode);
         throw new Error("error");
       }
 
