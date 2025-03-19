152e37921b7e5cfb99dbda7055f1a5afc6e923ad
wmb1207 <wmb1207@proton.me>
2025-03-19 09:53:25 -0300

    JSDOC

--- test.js
+++ test.js
@@ -1,58 +1,33 @@
 const https = require("https");
 const fetch = require("node-fetch");
+const utils = require("./utils.cjs");
 
 async function run() {
   try {
-    const message = `New PR created on Github TESTING BY LAO`;
-    const params = {
-      issue: {
-        notes: message,
-      },
-    };
+    const issueNumbers = [9196]
+    const percentageDone = 10;
+    for (const number of issueNumbers) {
+      const res = await utils.put({
+        hostname: "https://redmine.42mate.com",
+	apiKey: 'ec234c37b836236e0de1d91de607b301ed1eb370',
+        number: 9196,
+        action: "opened",
+        merged: false,
+        pr: {
+	  data: {
+	    title: "this is a test",
+	    url: "https://redmine.42mate.com/issues",
+	    body: "this is the best body event \n Javi needs to learn how to do this"
+	  }
+	},
+        percentage: percentageDone,
+      });
+      console.log(res);
+      if (res.status != 204) {
+        throw new Error(res);
+      }
+    }
 
-    var options = {
-      host: "redmine.42mate.com",
-      path: `/issues/9196.json`,
-      method: "PUT",
-      headers: {
-        "X-Redmine-API-Key": "ec234c37b836236e0de1d91de607b301ed1eb370",
-        "Content-type": "application/json",
-      },
-    };
-
-    const res = await fetch("https://redmine.42mate.com/issues/9196.json", {
-      method: "PUT",
-      headers: {
-        "X-redmine-api-key": "ec234c37b836236e0de1d91de607b301ed1eb370",
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(params),
-    });
-
-    console.log(res);
-
-    // var req = https.request(options, function (res) {
-    //   if (
-    //     res.statusCode !== 200 &&
-    //     res.statusCode !== 201 &&
-    //     res.statusCode !== 204
-    //   ) {
-    //     console.log(res.statusCode);
-    //     throw new Error("error");
-    //   }
-    //   console.log(res.statusCode);
-    // });
-
-    // req.on("error", function (err) {
-    //   console.log(options);
-    //   throw err;
-    // });
-
-    // var body = JSON.stringify(params);
-    // console.log(body);
-    // req.write(body);
-
-    // req.end();
   } catch (error) {
     console.error("error: " + error);
     process.exitCode = 1;
