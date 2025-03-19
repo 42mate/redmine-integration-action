293f304bfd4025dc5964f15181c8f1ab47fffdb9
wmb1207 <wmb1207@proton.me>
2025-03-19 15:45:46 -0300

    fix-missing-percentage

--- index.js
+++ index.js
@@ -165,6 +165,9 @@ async function run() {
         pr: pr,
         percentage: percentageDone,
       });
+
+      console.log(res.status, res.body);
+      
       if (res.status != 204) {
         throw new Error(res);
       }
